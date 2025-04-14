ba99967ea6e470522ac035cf4978eb0a49c05fdb
Shubhamhbtu <60982783+Shubhamhbtu@users.noreply.github.com>
2025-04-14 11:07:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ Use this file for all configurations
 
 ABUSEIPDB_API_KEY = "<ABUSEIPDB_API_KEY>"
 VT_API_KEY = "<VT_API_KEY>"
-MONGO_URI = "mongodb+srv://optiv_test:H8aD6NywcfdYfHBB@abusevt.wozixl5.mongodb.net/"
+MONGO_URI = "<MongoURI>"
 EMAIL_SENDER = "<EMAIL_SENDER>"
 EMAIL_PASSWORD = "<EMAIL_PASSWORD>"
 EMAIL_RECEIVER = "<EMAIL_RECEIVER>"
